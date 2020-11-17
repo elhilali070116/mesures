4e2cae43e93f6c1d1ed70e25c74a3bbc7c3e5bb6
elhilali070116 <elhilali070116@gmail.com>
2020-11-17 23:54:34 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,160 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    
+<!-- Mirrored from www.themezaa.com/html/pofo/home-creative-studio.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 18 Sep 2020 13:31:27 GMT -->
+<head>
+        <!-- title -->
+        <title>POFO – Creative Agency, Corporate and Portfolio Multi-purpose Template</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1" />
+        <meta name="author" content="ThemeZaa">
+        <!-- description -->
+        <meta name="description" content="POFO is a highly creative, modern, visually stunning and Bootstrap responsive multipurpose agency and portfolio HTML5 template with 25 ready home page demos.">
+        <!-- keywords -->
+        <meta name="keywords" content="creative, modern, clean, bootstrap responsive, html5, css3, portfolio, blog, agency, templates, multipurpose, one page, corporate, start-up, studio, branding, designer, freelancer, carousel, parallax, photography, personal, masonry, grid, coming soon, faq">
+        <!-- favicon -->
+        <link rel="shortcut icon" href="assetsPofo/images/favicon.png">
+        <link rel="apple-touch-icon" href="assetsPofo/images/apple-touch-icon-57x57.png">
+        <link rel="apple-touch-icon" sizes="72x72" href="assetsPofo/images/apple-touch-icon-72x72.png">
+        <link rel="apple-touch-icon" sizes="114x114" href="assetsPofo/images/apple-touch-icon-114x114.png">
+        <!-- animation -->
+        <link rel="stylesheet" href="assetsPofo/css/animate.css" />
+        <!-- bootstrap -->
+        <link rel="stylesheet" href="assetsPofo/css/bootstrap.min.css" />
+        <!-- et line icon --> 
+        <link rel="stylesheet" href="assetsPofo/css/et-line-icons.css" />
+        <!-- font-awesome icon -->
+        <link rel="stylesheet" href="assetsPofo/css/font-awesome.min.css" />
+        <!-- themify icon -->
+        <link rel="stylesheet" href="assetsPofo/css/themify-icons.css">
+        <!-- swiper carousel -->
+        <link rel="stylesheet" href="assetsPofo/css/swiper.min.css">
+        <!-- justified gallery -->
+        <link rel="stylesheet" href="assetsPofo/css/justified-gallery.min.css">
+        <!-- magnific popup -->
+        <link rel="stylesheet" href="assetsPofo/css/magnific-popup.css" />
+       
+        <!-- bootsnav -->
+        <link rel="stylesheet" href="assetsPofo/css/bootsnav.css">
+        <!-- style -->
+        <link rel="stylesheet" href="assetsPofo/css/style.css" />
+        <!-- responsive css -->
+        <link rel="stylesheet" href="assetsPofo/css/responsive.css" />
+        <!--[if IE]>
+            <script src="assetsPofo/js/html5shiv.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        
+       
+        <section class="wow fadeIn bg-extra-dark-gray">
+            <div class="container">
+                <div class="row"> 
+                    <div class="col-12 col-lg-6 padding-ten-right lg-padding-five-right md-padding-15px-right md-margin-five-bottom wow fadeIn">
+                        <p class="alt-font text-medium margin-15px-bottom">Technology expert people analysis</p>
+                        <h5 class="alt-font text-white-2 margin-20px-bottom">Feel free to get in touch with us any convenient way</h5>
+                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                        <div class="icon-box d-table w-100 mx-auto mx-lg-0 last-paragraph-no-margin">
+                            <div class="icon-box-holder align-middle d-table-cell position-relative">
+                                <i class="icon-map icon-medium text-deep-pink padding-5px-top"></i>
+                                <div class="alt-font text-white-2">Postal Address</div>
+                                <p>301 The Greenhouse, Custard, Factory, London, E2 8DY.</p>
+                            </div>
+                        </div>
+                        <div class="icon-box d-table w-100 mx-auto mx-lg-0 last-paragraph-no-margin">
+                            <div class="icon-box-holder align-middle d-table-cell position-relative">
+                                <i class="icon-envelope icon-medium text-deep-pink padding-5px-top"></i>
+                                <div class="alt-font text-white-2">General Enquiries</div>
+                                <p><a href="mailto:no-reply@domain.com"> no-reply@domain.com</a> / <a href="mailto:sales@domain.com">sales@domain.com</a></p>
+                            </div>
+                        </div>
+                        <div class="icon-box d-table w-100 mx-auto mx-lg-0 last-paragraph-no-margin">
+                            <div class="icon-box-holder align-middle d-table-cell position-relative">
+                                <i class="icon-chat icon-medium text-deep-pink padding-5px-top"></i>
+                                <div class="alt-font text-white-2">Business Phone</div>
+                                <p>+44 (0) 123 456 7890  /  +44 (0) 123 456 700</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-12 col-lg-6 wow fadeIn">
+                        <div class="padding-fifteen-all bg-white border-radius-6 lg-padding-seven-all">
+                            <div class="text-extra-dark-gray alt-font text-large font-weight-600 margin-30px-bottom">Looking for an excellent business idea?</div> 
+                            <div>
+                                <form id="contact-form" action="javascript:void(0)" method="post">
+                                    <div id="success-contact-form" class="mx-0"></div>
+                                    <input type="text" name="name" id="name" placeholder="Name*" class="input-bg">
+                                    <input type="text" name="email" id="email" placeholder="E-mail*" class="input-bg">
+                                    <input type="text" name="subject" id="subject" placeholder="Subject" class="input-bg">
+                                    <textarea name="comment" id="comment" placeholder="Your Message" class="input-bg"></textarea>
+                                    <button id="contact-us-button" type="submit" class="btn btn-small border-radius-4 btn-black">send message</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+       
+        <!-- javascript libraries -->
+        <script type="text/javascript" src="assetsPofo/js/jquery.js"></script>
+        <script type="text/javascript" src="assetsPofo/js/modernizr.js"></script>
+        <script type="text/javascript" src="assetsPofo/js/bootstrap.bundle.js"></script>
+        <script type="text/javascript" src="assetsPofo/js/jquery.easing.1.3.js"></script>
+        <script type="text/javascript" src="assetsPofo/js/skrollr.min.js"></script>
+        <script type="text/javascript" src="assetsPofo/js/smooth-scroll.js"></script>
+        <script type="text/javascript" src="assetsPofo/js/jquery.appear.js"></script>
+        <!-- menu navigation -->
+        <script type="text/javascript" src="assetsPofo/js/bootsnav.js"></script>
+        <script type="text/javascript" src="assetsPofo/js/jquery.nav.js"></script>
+        <!-- animation -->
+        <script type="text/javascript" src="assetsPofo/js/wow.min.js"></script>
+        <!-- page scroll -->
+        <script type="text/javascript" src="assetsPofo/js/page-scroll.js"></script>
+        <!-- swiper carousel -->
+        <script type="text/javascript" src="assetsPofo/js/swiper.min.js"></script>
+        <!-- counter -->
+        <script type="text/javascript" src="assetsPofo/js/jquery.count-to.js"></script>
+        <!-- parallax -->
+        <script type="text/javascript" src="assetsPofo/js/jquery.stellar.js"></script>
+        <!-- magnific popup -->
+        <script type="text/javascript" src="assetsPofo/js/jquery.magnific-popup.min.js"></script>
+        <!-- portfolio with shorting tab -->
+        <script type="text/javascript" src="assetsPofo/js/isotope.pkgd.min.js"></script>
+        <!-- images loaded -->
+        <script type="text/javascript" src="assetsPofo/js/imagesloaded.pkgd.min.js"></script>
+        <!-- pull menu -->
+        <script type="text/javascript" src="assetsPofo/js/classie.js"></script>
+        <script type="text/javascript" src="assetsPofo/js/hamburger-menu.js"></script>
+        <!-- counter -->
+        <script type="text/javascript" src="assetsPofo/js/counter.js"></script>
+        <!-- fit video -->
+        <script type="text/javascript" src="assetsPofo/js/jquery.fitvids.js"></script>
+
+        <!-- skill bars -->
+        <script type="text/javascript" src="assetsPofo/js/skill.bars.jquery.js"></script> 
+        <!-- justified gallery -->
+        <script type="text/javascript" src="assetsPofo/js/justified-gallery.min.js"></script>
+        <!--pie chart-->
+        <script type="text/javascript" src="assetsPofo/js/jquery.easypiechart.min.js"></script>
+        <!-- retina -->
+        <script type="text/javascript" src="assetsPofo/js/retina.min.js"></script>
+        <!-- revolution -->
+        <script type="text/javascript" src="revolution/js/jquery.themepunch.tools.min.js"></script>
+        <script type="text/javascript" src="revolution/js/jquery.themepunch.revolution.min.js"></script>
+        <!-- revolution slider extensions (load below extensions JS files only on local file systems to make the slider work! The following part can be removed on server for on demand loading) -->
+        <!--<script type="text/javascript" src="revolution/js/extensions/revolution.extension.actions.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.carousel.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.migration.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+        <script type="text/javascript" src="revolution/js/extensions/revolution.extension.video.min.js"></script>-->
+        <!-- setting -->
+        <script type="text/javascript" src="assetsPofo/js/main.js"></script>
+    </body>
+
+<!-- Mirrored from www.themezaa.com/html/pofo/home-creative-studio.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 18 Sep 2020 13:31:47 GMT -->
+</html>
